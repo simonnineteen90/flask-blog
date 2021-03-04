443e2e4a9c86a95cfaa9a72a6864f924e8679309
Simon Dunn <simondunn90@gmail.com>
2021-03-04 19:18:17 +0000

    connected to API to get data using Requests package

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+
+# API Request
+def get_api_data():
+    response = requests.get('https://api.npoint.io/43644ec4f0013682fc0d')
+    response.raise_for_status()
+    data = response.json()[0]["body"]
+    return data
+
+
 @app.route('/')
 def index():
+    print(get_api_data())
     return render_template('index.html')
 
 
